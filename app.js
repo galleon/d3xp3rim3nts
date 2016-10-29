179c10e2d18b510197d2c5f6a85e3a4a6d713ee2
galleon <guillaume.alleon@gmail.com>
2016-10-29 17:23:32 +0200

    - add showBarbs function - change HSLA so that a lighter box means fewer points - add a second data generator

--- app.js
+++ app.js
@@ -14,11 +14,35 @@ var svg = d3.select("#map").append("svg")
 /*
 var random = Math.random, data = d3.range(2500).map(function() { return [random() * width, random() * height]; });
 */
-var NUMBER_OF_POINTS = 100000;
+var NUMBER_OF_POINTS = 10000;
+//
+// sinus function
+
 var data = d3.range(NUMBER_OF_POINTS).map(function(value){
   return[value * 960 / NUMBER_OF_POINTS, Math.abs(Math.round(250 + 220 *
-    Math.sin(6 * Math.PI * value / NUMBER_OF_POINTS))), 1];
+    Math.sin(6 * Math.PI * value / NUMBER_OF_POINTS))), 10*Math.random(), 10*Math.random()];
 });
+
+// lorentz
+/*
+var x = 0.1;
+var y = 0.0;
+var z = 0.0;
+var dt = 0.01;
+var sigma = 10.0;
+var rho = 28.0;
+var beta = 8.0 / 3.0;
+
+var data = d3.range(NUMBER_OF_POINTS).map(function(value){
+  var dx = dt * sigma * (y - x);
+  var dy = dt * (x * (rho - z) - y);
+  var dz = dt * (x * y - beta * z);
+  x = x + dx;
+  y = y + dy;
+  z = z + dz;
+  return [x, y, dx, dy];
+});
+*/
 
 //var quadtree = d3.quadtree(null, function(d){return d[1]}, function(d){return d[0]})
 var quadtree = d3.quadtree()
@@ -40,25 +64,26 @@ function update(node, x0, y0, x1, y1){
   node.x1 = x1;
   node.y1 = y1;
   // loop on children
-  for(var i = 0; i < 4; i++) {
+  for (var i = 0; i < 4; i++) {
     // Will be false for leaves as they are not array
-    if(node[i]) {
+    if (node[i]) {
       count ++;
       // lazy init of new_data
-      if(!newData)
+      if (!newData)
         newData = new Array(node[i].data.length).fill(0);
 
       // accumulate children data (x, y, value)
-      for(var j = 0; j < newData.length; j++) {
+      for (var j = 0; j < newData.length; j++) {
         newData[j] += node[i].data[j];
       }
     }
   }
-  if(count) {
+  if (count) {
     // assuming that data[0] and data[1] are the coordinates
     // of the points, so we compute the mean
-    newData[0] /= count;
-    newData[1] /= count;
+    for (var j = 0; j < newData.length; j++) {
+        newData[j] = newData[j] / count;
+    }
     node.data = newData;
   }
 }
@@ -139,16 +164,105 @@ function showNodes(nodes) {
   // remove existing <rect>
   nodeSVGGroup.selectAll("rect").remove();
   // add rect matching the requested nodes
-  nodeSVGGroup.selectAll("rect").data(nodes)
-      .enter().append("rect")
-      .attr("x", function(d) { return d.x0; })
-      .attr("y", function(d) { return d.y0; })
-      .attr("width", function(d) { return d.y1 - d.y0; })
-      .attr("height", function(d) { return d.x1 - d.x0; })
-      .attr("style", function(node) {
-          var lightness  = 100 * Math.log(node.data[2]) / Math.log(data.length);
-          return "fill:hsla(180, 100%, " + lightness + "%, 128)";
-      });
+  var selection = nodeSVGGroup.selectAll("rect").data(nodes);
+
+  selection.enter()
+    .append("rect")
+    .attr("x", function(node) { return node.x0; })
+    .attr("y", function(node) { return node.y0; })
+    .attr("width", function(node) { return node.y1 - node.y0; })
+    .attr("height", function(node) { return node.x1 - node.x0; })
+    .attr("style", function(node) {
+      var lightness  = 100 - 100 * Math.log(node.data[2]) / Math.log(data.length);
+      return "fill:hsla(180, 100%, " + lightness + "%, 128)";
+    });
+}
+
+function showBarbs(nodes) {
+  nodeSVGGroup.selectAll(".barb").remove();
+
+  var selection = nodeSVGGroup.selectAll(".barb").data(nodes);
+
+  selection.enter()
+    .append('g')
+    .each(function(node, i) {
+      var vx = node.data[2];
+      var vy = node.data[3];
+
+      console.log('vx: '+ vx + ' vy:' + vy);
+  
+      var speed = Math.sqrt(vx*vx + vy*vy);
+      var angle = Math.atan2(-vy, vx) * (180/Math.PI);
+
+      console.log('speed: ' + speed + ', angle: ' + angle);
+
+      var index = 0, i;
+
+      var ten   = 0;
+      var five  = 0;
+      var fifty = 0;
+  
+      var path = "";
+      if (speed > 0) {
+        // Prepare the path
+        if (speed <= 7) {
+          path += "M0 2 L8 2 ";
+          index = 1;
+        } else {
+          path += "M1 2 L8 2 ";
+        }
+
+        // Find the number of lines in function of the speed
+        five = Math.floor(speed / 5);
+        if (speed % 5 >= 3) {
+          five += 1;
+        }
+
+        // Add triangles (5 * 10)
+        fifty = Math.floor(five / 10);
+        five -= fifty * 10;
+        // Add tenLines (5 * 2)
+        ten = Math.floor(five / 2);
+        five -= ten * 2;
+      }
+
+      var j;
+
+      // Draw first the triangles
+      for (i = 0; i < fifty; i++) {
+        j = index + 2 * i;
+        path += "M" + j + " 0 L" + (j + 1) + " 2 L" + j + " 2 L" + j + " 0 ";
+      }
+      if (fifty > 0) {
+        index += 2 * (fifty - 0.5);
+      }
+
+      // Draw the long segments
+      for (i = 0; i < ten; i++) {
+        j = index + i;
+        path += "M" + j + " 0 L" + (j + 1) + " 2 ";
+      }
+      index += ten;
+
+      // Draw the short segments
+      for (i = 0; i < five; i++) {
+        j = index + i;
+        path += "M" + (j + 0.5) + " 1 L" + (j + 1) + " 2 ";
+      }
+
+      path += "Z";
+
+      var tx = 0.5*(node.x0 + node.x1);
+      var ty = 0.5*(node.y1 + node.y0);
+
+      var scale = (node.x1 - node.x0) / 16;
+
+      d3.select(this).append('path')
+        .attr('d', function(d){console.log(d); return path;})
+        .attr('transform', 'translate(' + tx + ', ' + ty + ') scale(' + scale + ') rotate(' + angle + ' ' + 0 + ' ' + 0 + ')  translate(-4, -1)')
+        .attr('vector-effect', 'non-scaling-stroke');
+  })
+  .attr('class', 'wind-arrow');
 }
 
 var goalRangeInput = d3.select('#goal');
@@ -156,6 +270,8 @@ var goalValueSpan = d3.select('#goal-value');
 goalRangeInput.attr("max", treeDepth(quadtree));
 goalRangeInput.on('change', function() {
   var level = d3.select(this).property('value');
-  showNodes(nodesAtLevel(quadtree, level));
+  var front = nodesAtLevel(quadtree, level);
+  showNodes(front);
+  showBarbs(front);
   goalValueSpan.text(level);
 });
